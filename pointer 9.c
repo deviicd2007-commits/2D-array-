771b157bb8d2985b4805b558e1d6c019b6b53f06
deviicd2007-commits <deviicd2007@gmail.com>
2026-03-26 09:33:24 +0530

    Add files via upload

---
+++ pointer 9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,x;
+    int arr[100];
+    int *ptr;
+    int pos = -1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&x);
+    ptr = arr;
+    for(i=0;i<n;i++)
+    {
+        if(*(ptr+i) == x)
+        {
+            pos = i + 1;
+            break;
+        }
+    }
+    printf("%d",pos);
+    return 0;
+}
